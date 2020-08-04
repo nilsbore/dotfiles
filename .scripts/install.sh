c7d13b4d8334b3c1989613e65beca023f1a25a20
Nils Bore <nbore@kth.se>
2020-08-04 20:27:14 +0200

    Updated install script with repo init

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,3 +1,8 @@
+cd $HOME
+
+mkdir -p .installs
+mkdir -p .fonts
+
 # Get some nerd font, e.g.
 # https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete.ttf
 # https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.t
@@ -17,13 +22,22 @@ omf install bobthefish
 omf install bass
 
 # Install the nord Gnome terminal theme
-cd ~/Installs
 sudo apt install dconf-tools dconf-gsettings-backend dconf-cli dconf-service uuid-runtime
+
+cd .installs
 git clone https://github.com/arcticicestudio/nord-gnome-terminal.git
 cd nord-gnome-terminal/src
 ./nord.sh
+cd $HOME
 
 # TODO: now you need to go the the settings and set the Nord profile as the default
 
 # Set fish as the default shell
 chsh -s /usr/bin/fish
+
+# This might cause problems and will therefor run last
+git clone --bare https://github.com/nilsbore/dotfiles.git $HOME/.dotfiles
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+dotfiles config --local status.showUntrackedFiles no
+dotfiles checkout master
+
