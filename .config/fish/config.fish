d80cbb7cc116deac9f4b8f6fa561865102a8b409
Nils Bore <nbore@kth.se>
2020-08-04 14:18:09 +0200

    Added dotfiles alias to config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme nord
 source /opt/ros/melodic/share/rosbash/rosfish
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
