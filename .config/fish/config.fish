c5bebd2e1887826dcd4f28d257061f3c32bf4b0b
Nils Bore <nilsbore@gmail.com>
2022-12-08 12:03:19 +0100

    Improved experience on WSL

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
 set -g theme_nerd_fonts yes
-set -g theme_color_scheme nord
+set -g theme_color_scheme terminal-dark
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+set --export PATH $HOME/.local/bin $HOME/.cargo/bin $PATH
+set --export COLORTERM truecolor
 
 # if ros support is desired
 # bass source /opt/ros/melodic/setup.bash
